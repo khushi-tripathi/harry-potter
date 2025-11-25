5ddce637389e907e0d5dc37c592b2ac7e41750fe
Khushi Tripathi <khushitripathi004@gmail.com>
2025-11-25 17:40:02 +0530

    Summary : Showing four houses on the screen by the api call

--- api_integration.js
+++ api_integration.js
@@ -1,6 +1,6 @@
 // API Integration
 
-import { BOOKS } from "./constant.js";
+import { BOOKS, CHARACTERS, HOUSES, SPELLS, STAFF } from "./constant.js";
 
 async function getApiIntegration(url) {
   const res = await fetch(url);
@@ -8,9 +8,54 @@ async function getApiIntegration(url) {
   return result;
 }
 
+async function fetchHouseData(data) {
+  const houses = data.map((item, idx) => {
+    return item?.house;
+  });
+  return houses;
+}
+async function fetchDataByName(name, data) {
+  let result = {};
+  switch (name) {
+    case "house":
+      result = fetchHouseData(data);
+      break;
+
+    default:
+      break;
+  }
+  return result;
+}
+
+async function callApi(name) {
+  let url = "";
+  switch (name) {
+    case "book":
+      url = BOOKS;
+      break;
+    case "house":
+      url = HOUSES;
+      break;
+    case "professor":
+      url = STAFF;
+      break;
+    case "character":
+      url = CHARACTERS;
+      break;
+    case "spell":
+      url = SPELLS;
+      break;
+
+    default:
+      break;
+  }
+  const data = await getApiIntegration(url);
+  const result = await fetchDataByName(name, data);
+  return result;
+}
 function allStudentDetails() {
   console.log("Khushiiii");
   return ["Harry", "Lisa", "Snape"];
 }
 
-export { allStudentDetails, getApiIntegration };
+export { allStudentDetails, getApiIntegration, callApi };
--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-import { allStudentDetails } from "./api_integration.js";
+import { allStudentDetails, callApi } from "./api_integration.js";
 
 // alert("Hi Potter");
 
+let targetDiv = {};
 const element = document.querySelector("#clickMe");
 element.addEventListener("click", (e) => {
   onclickStudents();
+});
+
+const sub_section = document.querySelectorAll(".sub-section");
+// sub_section.addEventListener("click", (event) => {
+//   console.log("On click of feature : ", event);
+// });
+
+sub_section.forEach((section) => {
+  section.addEventListener("click", async (event) => {
+    console.log("On click -  : ", event);
+    targetDiv = event.target.value;
+    const data = await callApi(event.target.value);
+    const ele = document.querySelector("#" + event.target.value + "s");
+    data?.map((item) => {
+      const div = document.createElement("div");
+      div.textContent = item;
+      ele?.append(div);
+    });
+    // ele.textContent = data;
+    // .map((house) => { return (<div> {house}</div>)})
+    // debugger;
+  });
 });
 
 const onclickStudents = () => {
